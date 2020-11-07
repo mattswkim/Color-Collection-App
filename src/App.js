66f546a7e1b1caed561577b95661036d2e8221a4
Matt Kim <mattsunwoongkim@gmail.com>
2020-11-07 14:44:41 +0900

    Add Router and Palette List

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import { Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedPalette from './seedPalette';
 import { generatePalette } from './colorHelpers'
 
 class App extends Component {
+  findPalette(id) {
+    return seedPalette.find((palette) => {
+      return palette.id === id;
+    });
+  }
   render() {
 
     return (
-      <div>
-        <Palette palette={generatePalette(seedPalette[4])} />
-      </div>
-    )
+      <Switch>
+        <Route 
+          exact 
+          path='/' 
+          render={() => 
+            <PaletteList palettes={seedPalette}/>
+          }
+        />
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={(routeProps) => (
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>
+          )}
+        />
+      </Switch>
+      //<div>
+      //  <Palette palette={generatePalette(seedPalette[4])} />
+      //</div>
+    );
   }
 }
 
--- src/Palette.js
+++ src/Palette.js
@@ -14,16 +14,15 @@ class Palette extends Component {
   changeLevel(level) {
     this.setState({ level })
   }
-  
   changeFormat(val) {
     this.setState({ format: val })
   }
 
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id}/> 
     ))
     return (  
       <div className="Palette">
@@ -31,11 +30,13 @@ class Palette extends Component {
       changeLevel={this.changeLevel} 
       level={level} 
       handleChange={this.changeFormat}/>
-      {/* Navbar goes here */}
         <div className="Palette-colors">
           {colorBoxes}
         </div>
-        {/* footer */}
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     )
   }
