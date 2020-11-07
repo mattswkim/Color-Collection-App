bd187c7b8f08aeb176cf77e32f6350f09524fdc2
Matt Kim <mattsunwoongkim@gmail.com>
2020-11-07 09:30:58 +0900

    Add sncakbar

--- src/Palette.js
+++ src/Palette.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 
 class Palette extends Component {
   constructor (props) {
     super(props);
-    this.state= { level: 500 };
+    this.state= { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level })
   }
+  
+  changeFormat(val) {
+    this.setState({ format: val })
+  }
+
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ))
-    return (
+    return (  
       <div className="Palette">
-      <Slider 
-        defaultValue={level} 
-        min={100} 
-        max={900}
-        step={100}
-        onAfterChange={this.changeLevel}
-      />
+      <Navbar 
+      changeLevel={this.changeLevel} 
+      level={level} 
+      handleChange={this.changeFormat}/>
       {/* Navbar goes here */}
         <div className="Palette-colors">
           {colorBoxes}
